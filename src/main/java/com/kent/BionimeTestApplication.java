eb2ca066ac20de0eb4e4deb360ef0170361c6dd5
Zijun <s410126080@gmail.com>
2023-07-22 21:29:25 +0800

    initialize the project and load relative dependencies

---
+++ src/main/java/com/kent/BionimeTestApplication.java
@@ -0,0 +1,13 @@
+package com.kent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BionimeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BionimeTestApplication.class, args);
+    }
+
+}
