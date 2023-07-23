74ef0898c963cc1ab3a782e32736ac8c37dfead5
Zijun <s410126080@gmail.com>
2023-07-23 16:20:10 +0800

    add the modification_time property to the Nurse entity class.

--- src/main/java/com/kent/domain/Nurse.java
+++ src/main/java/com/kent/domain/Nurse.java
@@ -3,6 +3,8 @@ package com.kent.domain;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Entity
 @Table(name = "nurse")
@@ -22,4 +24,21 @@ public class Nurse {
     @ManyToOne
     @JoinColumn(name = "station_id", referencedColumnName = "id")
     private MedicalAidStation station;
+
+    @Column(name = "modification_time", columnDefinition = "TIMESTAMP")
+    private LocalDateTime modificationTime;
+
+    @PrePersist
+    protected void onCreate() {
+        // 新增資料前，設置 modificationTime 屬性為當前日期時間
+        this.modificationTime = LocalDateTime.now();
+        System.out.println(this.modificationTime);
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        // 更新資料前，設置 modificationTime 屬性為當前日期時間
+        this.modificationTime = LocalDateTime.now();
+        System.out.println(this.modificationTime);
+    }
 }
